42569dc576bc2d522c11b09ca089cbc982d353ad
Edson Ayllon <eayllon1@gmail.com>
2020-04-22 22:33:12 -0500

    modified daoImpl

--- src/main/java/com/ecommerce/dao/CommerceDAOImpl.java
+++ src/main/java/com/ecommerce/dao/CommerceDAOImpl.java
@@ -3,9 +3,16 @@ package com.ecommerce.dao;
 import com.ecommerce.models.User;
 
 public class CommerceDAOImpl implements CommerceDAO {
+	
+	@PersistenceContext
+	private EntityManager entityManager;
+	
+	@Override
 	public void addUser(User user) throws Exception {
 		
 	}
+	
+	@Override
 	public User getUser(String email, String password) throws Exception {
 		return null;
 	}
