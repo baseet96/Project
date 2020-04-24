7ea268dfece333596dea5318a2a97612f412dc92
Basit Zahid <basit.zahid@yahoo.com>
2020-04-23 23:29:05 -0500

    Added Cart entity with Product mapped as @OneToMany Added addProduct.jsp for getting input from admin to add product Modified admin page to include add product Added controller for adding product

---
+++ src/main/java/com/ecommerce/controllers/AddProductController.java
@@ -0,0 +1,20 @@
+package com.ecommerce.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class AddProductController {
+	
+	@GetMapping("/addProduct")
+	public String showHomePage() {
+		
+		System.out.println("In Add Product Page");
+
+		
+		return "addProduct";
+	}
+
+}
