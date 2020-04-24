2da814cce943355a9dfe382de900af9e7e451d0c
Edson Ayllon <eayllon1@gmail.com>
2020-04-23 20:35:00 -0500

    Fill dao for authentication, first version

--- src/main/java/com/ecommerce/dao/CommerceDAO.java
+++ src/main/java/com/ecommerce/dao/CommerceDAO.java
@@ -3,6 +3,6 @@ package com.ecommerce.dao;
 import com.ecommerce.models.User;
 
 public interface CommerceDAO {
-	public void addUser(User user) throws Exception;
-	public User getUser(String email, String password) throws Exception;
+	public void registerUser(User user) throws Exception;
+	public User loginUser(String email, String password) throws Exception;
 }
