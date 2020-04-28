f419221ae8d0f3ce63e288faa81d7df8a90196d6
Sam Landsman <shalomlandsman@gmail.com>
2020-04-27 23:27:18 -0500

    modify cart model

--- src/main/java/com/ecommerce/models/Cart.java
+++ src/main/java/com/ecommerce/models/Cart.java
@@ -2,61 +2,87 @@ package com.ecommerce.models;
 
 import java.util.List;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
-
+import com.ecommerce.entities.CartEntity;
 import com.ecommerce.entities.ProductEntity;
 import com.ecommerce.entities.UserEntity;
 
-
-@Entity
 public class Cart {
-	
-	
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private Integer cartId;
-	
-	
-	@OneToMany
-	private List<ProductEntity> products;
-	
-	@OneToOne
-	private UserEntity user;
-	
-	
-	private double discount;
-	private double total;
-	
 
-	public List<ProductEntity> getProducts() {
-		return products;
-	}
-	public void setProduct(List<ProductEntity> products) {
-		this.products = products;
-	}
-	public UserEntity getUser() {
-		return user;
-	}
-	public void setUser(UserEntity user) {
-		this.user = user;
-	}
-	public double getDiscount() {
-		return discount;
-	}
-	public void setDiscount(double discount) {
-		this.discount = discount;
-	}
-	public double getTotal() {
-		return total;
-	}
-	public void setTotal(double total) {
-		this.total = total;
-	}
+    private Integer cartId;
+
+    private List<ProductEntity> products;
+
+    private UserEntity user;
+
+    private double discount;
+    private double total;
+
+    public Integer getCartId() {
+        return cartId;
+    }
+
+    public void setCartId(Integer cartId) {
+        this.cartId = cartId;
+    }
+
+    public List<ProductEntity> getProducts() {
+        return products;
+    }
+
+    public void setProduct(List<ProductEntity> products) {
+        this.products = products;
+    }
+
+    public UserEntity getUser() {
+        return user;
+    }
+
+    public void setUser(UserEntity user) {
+        this.user = user;
+    }
+
+    public double getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(double discount) {
+        this.discount = discount;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public void setTotal(double total) {
+        this.total = total;
+    }
+
+    @Override
+    public String toString() {
+        return "Product DTO [products=" + products + ", user=" + user + ", discount=" + discount + ", total="
+                + total + "]";
+    }
+
+    // converts entity Into Model
+    public static Cart valueOf(CartEntity cartEntity) {
+        Cart cart = new Cart();
+        cart.setProduct(cartEntity.getProducts());
+        cart.setUser(cartEntity.getUser());
+        cart.setDiscount(cartEntity.getDiscount());
+        cart.setTotal(cartEntity.getTotal());
+        
+
+        return cart;
+    }
+
+    // converts model Into Entity
+    public CartEntity createEntity() {
+        CartEntity cartEntity = new CartEntity();
+        cartEntity.setProduct(this.getProducts());
+        cartEntity.setUser(this.getUser());
+        cartEntity.setDiscount(this.getDiscount());
+        cartEntity.setTotal(this.getTotal());
+
+        return cartEntity;
+    }
 }
