2b8b20147b55eecb5ae224ec02906ec2a6dfc622
Edson Ayllon <eayllon1@gmail.com>
2020-04-27 20:47:14 -0500

    fill contextLoad test

--- src/test/java/com/verizon/project/ProjectApplicationTests.java
+++ src/test/java/com/verizon/project/ProjectApplicationTests.java
@@ -1,13 +1,26 @@
 package com.verizon.project;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.ecommerce.controllers.AuthController;
+import com.ecommerce.controllers.ProductController;
 
 @SpringBootTest
 class ProjectApplicationTests {
 
+	@Autowired
+	private AuthController authController;
+	
+	@Autowired ProductController productController;
+
 	@Test
-	void contextLoads() {
+	public void contexLoads() throws Exception {
+		assertNotNull(authController);
+		assertNotNull(productController);
 	}
 
 }
