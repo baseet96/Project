2f7b9c13baf40b17102abcd108b84f2cc979caa0
Edson Ayllon <eayllon1@gmail.com>
2020-04-27 21:59:47 -0500

    Add valid login test cases

--- src/test/java/com/verizon/project/ProjectApplicationTests.java
+++ src/test/java/com/verizon/project/ProjectApplicationTests.java
@@ -1,22 +1,50 @@
 package com.verizon.project;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.junit.Rule;
 import org.junit.jupiter.api.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import com.ecommerce.controllers.AuthController;
 import com.ecommerce.controllers.ProductController;
+import com.ecommerce.dao.CommerceDAO;
+import com.ecommerce.dao.CommerceDAOImpl;
+import com.ecommerce.models.User;
+import com.ecommerce.models.UserKind;
+import com.ecommerce.services.AuthService;
 
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class ProjectApplicationTests {
 
+	@Rule
+	public ExpectedException expectedException = ExpectedException.none();
+	
+	
 	@Autowired
 	private AuthController authController;
 	
 	@Autowired 
 	private ProductController productController;
+	
+	@Mock
+	CommerceDAO commerceDao = new CommerceDAOImpl();
+
+	@Autowired
+	AuthService authService;
+	
+	private SimpleDateFormat ft = new SimpleDateFormat ("yyyy-MM-dd"); 
 
 	@Test
 	public void contexLoads() throws Exception {
@@ -25,23 +53,63 @@ class ProjectApplicationTests {
 	}
 	
 	@Test 
-	public void registersUser() throws Exception {
+	public void validRegistration() throws Exception {
 		
 	}
 	
 	@Test 
-	public void logsUserIn() throws Exception {
+	public void invalidRegistration() throws Exception {
 		
 	}
 	
 	@Test 
-	public void loginAdmin() throws Exception {
+	public void validLogin() throws Exception {
+		String email = "customer@infosys.com";
+		String password = "password";
+		User user = new User();
+		user.setEmail(email);
+		user.setName("Bill Billiams");
+		user.setKind(UserKind.CUSTOMER);
+		user.setDob(this.ft.parse("1990-01-01"));
+		when(commerceDao.getUser("customer@infosys.com")).thenReturn(user);
+		when(commerceDao.loginUser("customer@infosys.com", "password")).thenReturn(user);
+		User actual = authService.loginUser(email, password);
+		assertEquals(user, actual);
+	}
+	
+	@Test 
+	public void invalidLogin() throws Exception {
 		
+	}
+	
+	@Test 
+	public void validAdminLogin() throws Exception {
+		String email = "admin@infosys.com";
+		String password = "passsword";
+		User user = new User();
+		user.setEmail(email);
+		user.setName("Sam Williams");
+		user.setKind(UserKind.ADMIN);
+		user.setDob(this.ft.parse("1990-01-01"));
+		when(commerceDao.getUser("admin@infosys.com")).thenReturn(user);
+		when(commerceDao.loginUser("admin@infosys.com", "password")).thenReturn(user);
+		User actual = authService.loginUser(email, password);
+		assertEquals(UserKind.ADMIN, actual.getKind());
 	}
 	
 	@Test
-	public void loginCustomer() throws Exception {
-		
+	public void validCustomerLogin() throws Exception {
+		String email = "customer@infosys.com";
+		String password = "password";
+		User user = new User();
+		user.setEmail(email);
+		user.setName("Bill Billiams");
+		user.setKind(UserKind.CUSTOMER);
+		user.setDob(this.ft.parse("1990-01-01"));
+		when(commerceDao.getUser("customer@infosys.com")).thenReturn(user);
+		when(commerceDao.loginUser("customer@infosys.com", "password")).thenReturn(user);
+		User actual = authService.loginUser(email, password);
+		assertEquals(UserKind.CUSTOMER, actual.getKind());
 	}
 	
 	@Test
