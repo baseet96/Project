3e5e0c9895d82aa40403af04b747ce74ca30eafb
Sam Landsman <shalomlandsman@gmail.com>
2020-04-27 23:24:20 -0500

    add cors annotation to auth controller to enable access to endpoint from frontend

--- src/main/java/com/ecommerce/controllers/AuthController.java
+++ src/main/java/com/ecommerce/controllers/AuthController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -21,6 +22,7 @@ import com.ecommerce.services.AuthService;
 import com.ecommerce.services.ProductService;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/")
 public class AuthController {
 	
